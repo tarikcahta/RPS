190d5a24c10f524f9fd0d6e52d290dfe06d3eb7b
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-02-10 13:25:40 +0100

    spacing changes

--- rps.js
+++ rps.js
@@ -1,16 +1,15 @@
-const options = ["rock", "paper", "scissors"]
-
+const options = ["rock", "paper", "scissors"];
 
 function computerPlay() {
-  let choice = options[Math.floor(Math.random()*options.length)];
-  return choice
+  let choice = options[Math.floor(Math.random() * options.length)];
+  return choice;
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
 function theGame() {
-  while (playerScore < 5 && computerScore <5) {
+  while (playerScore < 5 && computerScore < 5) {
     let playerSelection = prompt("Enter you poison : ");
     let computerSelection = computerPlay();
     playerSelection = playerSelection.toLowerCase();
@@ -29,50 +28,52 @@ console.log(theGame());
 //     console.log('Current score: Human: ', humanScore, ' Computer: ', computerScore)
 //     setTimeout(initializeGame, 0);
 //   } else {
-  //     console.log(win_condition());
-  //   }
-  // } 
-
+//     console.log(win_condition());
+//   }
+// }
 
 function playRound(playerSelection, computerSelection) {
-    switch (playerSelection){
-      case "rock":
-        if(computerSelection === "scissors") {
-          console.log("YOU have won!");
-          playerScore += 1;
-        }else if (playerSelection == computerSelection) { 
-          console.log("It's a tie");
-        }else { console.log("The Machine has won!"); 
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "scissors") {
+        console.log("YOU have won!");
+        playerScore += 1;
+      } else if (playerSelection == computerSelection) {
+        console.log("It's a tie");
+      } else {
+        console.log("The Machine has won!");
         computerScore += 1;
-        } 
-        break
-      case "paper":
-        if(computerSelection === "rock") {
-          console.log("YOU have won!");
-          playerScore += 1;
-        } else if (playerSelection == computerSelection) {
-          console.log("It's a tie!");
-        }else {console.log("The Machine has won!");
+      }
+      break;
+    case "paper":
+      if (computerSelection === "rock") {
+        console.log("YOU have won!");
+        playerScore += 1;
+      } else if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+      } else {
+        console.log("The Machine has won!");
         computerScore += 1;
-        }
-        break
-        case "scissors":
-          if (computerSelection === "paper"){
-            console.log("YOU have won!");
-            playerScore += 1;
-          }else if (playerSelection == computerSelection){
-            console.log("It's a tie!!!");
-          }else {console.log("The Machine has won!");
-          computerScore += 1;
-          }break
-    }
+      }
+      break;
+    case "scissors":
+      if (computerSelection === "paper") {
+        console.log("YOU have won!");
+        playerScore += 1;
+      } else if (playerSelection == computerSelection) {
+        console.log("It's a tie!!!");
+      } else {
+        console.log("The Machine has won!");
+        computerScore += 1;
+      }
+      break;
   }
+}
 
 function winCondition() {
   if (playerScore == 5) {
     return console.log("You HAVE WON!");
-  }
-  else if (computerScore == 5) {
+  } else if (computerScore == 5) {
     return console.log("The Machine has WON!");
   }
 }
